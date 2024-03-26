ed2c088c027894ae1c918218839094cfecc99ae1
Subham Sharma <81345739+subham-spec@users.noreply.github.com>
2024-03-26 12:16:36 +0000

    [Multiple Inheritence]

---
+++ python/OOPS/inheritence/02.py
@@ -0,0 +1,19 @@
+# Multiple Inheritence
+
+class Car:
+    def __init__(self, model, brand):
+        self.brand = brand
+        self.model = model
+
+class Car_Specs:
+    def __init__(self, range):
+        self.range = range
+     
+class Petrol_car(Car, Car_Specs):
+    def __init__(self, model, brand, fuel, range):
+        Car.__init__(self, model, brand)
+        Car_Specs.__init__(self, range)
+        self.fuel = fuel
+
+if __name__ == "__main__":
+    car1 = Petrol_car("Harrier","Tata","Petrol",736)
