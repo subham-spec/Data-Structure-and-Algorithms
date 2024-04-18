be98e9ed7f70f503c5dd1fcb9f36045ebc6dd96c
subham-spec <subham.pandit@hotmail.com>
2024-04-18 14:22:07 +0530

    [Island Perimeter]

---
+++ data_structures/array/easy/463.py
@@ -0,0 +1,61 @@
+# 463. Island Perimeter
+
+from typing import List
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        m, n = len(grid)-1, len(grid[0])-1
+
+        for i in range(0, m+1):
+            for j in range(0, n+1):
+                print(i, j)
+                if grid[i][j] == 0:
+                    continue
+                # Top block
+                if i==0 or grid[i-1][j]==0:
+                    perimeter += 1
+                # Right block
+                if j==n or grid[i][j+1]==0:
+                    perimeter += 1
+                # Bottom Block
+                if i==m or grid[i+1][j]==0:
+                    perimeter += 1
+                # Left Block
+                if j==0 or grid[i][j-1]==0:
+                    perimeter += 1
+
+        return perimeter
+
+'''
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+Example 2:
+
+Input: grid = [[1]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0]]
+Output: 4
+ 
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 100
+grid[i][j] is 0 or 1.
+There is exactly one island in grid.
+'''
