cda4b9b4fc92c90cca2397ba51de8d753bc2d460
subham-spec <subham.pandit@hotmail.com>
2024-06-23 21:32:11 +0530

    [DP_1 Fibonacci Series]

---
+++ dynamic_programming/1_fibonacci.py
@@ -0,0 +1,10 @@
+def nth_fibonacci(n):
+    pass
+
+if __name__ == "__main__":
+    n = int(input("Enter the number: "))
+    print("Nth number is: ",nth_fibonacci(n))
+
+
+
+# Overlapping subproblems -> Solutio
